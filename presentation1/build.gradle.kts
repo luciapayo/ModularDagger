139c66027ceac4adb8226fceb22154248ea241d7
Lucia Payo <lucia.payo@n26.com>
2018-11-16 14:47:55 +0100

    Add presentation1 module

---
+++ presentation1/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.feature")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdkVersion(AndroidVersions.compileSdkVersion)
+
+    defaultConfig {
+        minSdkVersion(AndroidVersions.minSdkVersion)
+        targetSdkVersion(AndroidVersions.targetSdkVersion)
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        getByName("release") {}
+    }
+}
+
+dependencies {
+}
